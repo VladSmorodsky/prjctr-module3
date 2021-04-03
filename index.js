18a034f8e698e1e1ec53c1d59dbc9bb5d4fdffd2
Vlad Smorodsky <smorodsky.odesa@gmail.com>
2021-04-03 12:24:51 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -3,60 +3,33 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    
+    let resultObj = {
+        status: 404,
+        text: 'Not found'
+    }    
     if(req.method == 'GET') {
         let urlSite = `http://${hostname}:${port}${req.url}`;
         let requestUrl = new URL(urlSite);
         let pathName = requestUrl.pathname;
         let name = requestUrl.searchParams.get('name');
 
-        switch(pathName) {
-            case '/hello' :
-                if(name !== null ){
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type','text/plain');
-                    res.end(`Hello ${name}`);
-                    return;
-                } else if (Array.from(requestUrl.searchParams).length > 0) {
-                    res.statusCode = 404;
-                    res.setHeader('Content-Type','text/plain'); 
-                    res.end('Not found');
-                    return;
-                } else {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type','text/plain');
-                    res.end(`Hello world`);
-                    return;
-                }
-                
-            case '/goodbye' :
-                if(name !== null ){
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type','text/plain');
-                    res.end(`Hello ${name}`);
-                    return;
-                } else if (Array.from(requestUrl.searchParams).length > 0) {
-                    res.statusCode = 404;
-                    res.setHeader('Content-Type','text/plain'); 
-                    res.end('Not Found');
-                    return;
-                } else {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type','text/plain');
-                    res.end(`Hello world`);
-                    return;
-                }  
-            default:
-                res.statusCode = 404;
-                res.setHeader('Content-Type','text/plain'); 
-                res.end('Not found');
-        } 
+        if(pathName == '/hello' || pathName == '/goodbye') {
+            if(name !== null ){
+                resultObj.status = 200;
+                resultObj.text = `Hello ${name}`;
+            } else if (Array.from(requestUrl.searchParams).length > 0) {
+                resultObj.status = 404;
+                resultObj.text = 'Not found';
+            } else {
+                resultObj.status = 200;
+                resultObj.text = 'Hello world';
+            }
+        }
     }
-    else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type','text/plain'); 
-        res.end('Not found');
-    }   
+    res.statusCode = resultObj.status;
+    res.setHeader('Content-Type','text/plain');
+    res.end(resultObj.text); 
+    return;  
 });
 
 server.listen(port, () => {
