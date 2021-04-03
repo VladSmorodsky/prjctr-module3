c47a195b3380b345e55bdf7eccd572423ed30078
Vlad Smorodsky <smorodsky.odesa@gmail.com>
2021-04-03 12:03:17 +0300

    Remove unnessesary log() from code

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
-const url = require('url');
 
 const server = http.createServer((req, res) => {
-
-    console.log(req);
     
     if(req.method == 'GET') {
         let urlSite = `http://${hostname}:${port}${req.url}`;
         let requestUrl = new URL(urlSite);
         let pathName = requestUrl.pathname;
         let name = requestUrl.searchParams.get('name');
-        console.log(Array.from(requestUrl.searchParams).length);
+
         switch(pathName) {
             case '/hello' :
                 if(name !== null ){
@@ -21,9 +18,9 @@ const server = http.createServer((req, res) => {
                     res.end(`Hello ${name}`);
                     return;
                 } else if (Array.from(requestUrl.searchParams).length > 0) {
-                    res.statusCode = 400;
+                    res.statusCode = 404;
                     res.setHeader('Content-Type','text/plain'); 
-                    res.end('Bad Request');
+                    res.end('Not found');
                     return;
                 } else {
                     res.statusCode = 200;
@@ -39,9 +36,9 @@ const server = http.createServer((req, res) => {
                     res.end(`Hello ${name}`);
                     return;
                 } else if (Array.from(requestUrl.searchParams).length > 0) {
-                    res.statusCode = 400;
+                    res.statusCode = 404;
                     res.setHeader('Content-Type','text/plain'); 
-                    res.end('Bad Request');
+                    res.end('Not Found');
                     return;
                 } else {
                     res.statusCode = 200;
@@ -56,9 +53,9 @@ const server = http.createServer((req, res) => {
         } 
     }
     else {
-        res.statusCode = 400;
+        res.statusCode = 404;
         res.setHeader('Content-Type','text/plain'); 
-        res.end('Bad Request');
+        res.end('Not found');
     }   
 });
 
